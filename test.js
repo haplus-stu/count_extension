6db4ff786823e241448c1d21fc7e2f33e2d0b19e
Vagrant Default User <syu8.0324@outlook.jp>
2020-04-10 09:10:05 +0900

    任意の文字数制限機能追加

--- test.js
+++ test.js
@@ -1,23 +1,32 @@
-function str() {
-    let str = document.getElementById("inputarea").value;
-    return str.replace(/\n+/g,"");
-   }
- 
- 
-  function number() { 
-   let counting_number = str().length;
-   if(counting_number >= 0){
-     document.getElementById("counter").innerHTML = counting_number+"文字";
-  }
- }
- 
- 
- function count(){
-   number();
- }
+'use strict';
 
-$(document).ready(function(){
-  $("#inputarea").on('keyup',function(e){
-    number();
-  });
-});
+let textarea = document.getElementById('inputarea');
+
+function str() {
+  let str = document.getElementById("inputarea").value;
+  return str.replace(/\n+/g, "");
+}
+
+
+function number() {
+  let max = document.getElementById('max-count').value;
+  let counting_number = str().length;
+  if (max != '' && 10 > max - counting_number) {
+    textarea.classList.add('close_max_count');
+  } else if (max != '' && 10 <= max - counting_number) {
+    textarea.classList.remove('close_max_count');
+  }
+
+  if (counting_number >= 0) {
+    document.getElementById("counter").innerHTML = counting_number + "文字";
+  }
+}
+
+
+function count() {
+  number();
+}
+
+textarea.onkeyup = function () {
+  number();
+}
